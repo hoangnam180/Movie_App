77fbc4b420f02469a632e1d1485dd6588d69a2a5
Hoàng Nam <nnam44565@gmail.com>
2022-05-08 03:34:51 +0700

    done

--- src/App.js
+++ src/App.js
@@ -9,7 +9,27 @@ import { Spin } from "antd";
 import Footer from "./Components/footer/Footer";
 import { UserContext } from "./context/itemmovie-context";
 import { publicRoutes } from "./routes";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAFyxGQCSj2AwOdw-tDuz-DI9LTn4QSMa0",
+  authDomain: "movieapp-6291c.firebaseapp.com",
+  projectId: "movieapp-6291c",
+  storageBucket: "movieapp-6291c.appspot.com",
+  messagingSenderId: "121487576331",
+  appId: "1:121487576331:web:4f89930e7caed1b8e4a60e",
+  measurementId: "G-PTXRVM03RX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 function App() {
   const [loading, setLoading] = useState(true);
   const { loadingPage, type } = useContext(UserContext);
